ea1211422da7c9ac0fe362233d1bc5cc7b29bfd4
Austin Martin <amartin@amartinsec.com>
2021-09-11 22:32:23 -0400

    Some Art

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import colorama
+from colorama import Fore, Style
+
+def art():
+    print(Fore.RED + "       , ,, ," + Style.RESET_ALL)
+    print(Fore.RED +"       | || | "+ Style.RESET_ALL + "   ,/  _____  \.")
+    print(Fore.RED +"       \_||_/  "+ Style.RESET_ALL + "  ||_/     \_||")
+    print(Fore.RED +"         ||   "+ Style.RESET_ALL + "    \_| . . |_/")
+    print(Fore.RED +"         ||   "+ Style.RESET_ALL + "      |  L  |")
+    print("        " + Style.RESET_ALL +"," + Fore.RED + "||    "+ Style.RESET_ALL + "     |`==='|")
+    print("        |>" + Fore.RED + "|"+ Style.RESET_ALL + "\    / "+ Fore.RED + " MSHTMHell" + Style.RESET_ALL +"  \\")
+    print("        \>" + Fore.RED + "| " + Style.RESET_ALL + "\  /  ,    .    .  |")
+    print(Fore.RED +"         || "+ Style.RESET_ALL + " \/  /| .  |  . |  |")
+    print(Fore.RED +"         ||"+ Style.RESET_ALL + "\  ` / | ___|___ |  |    " + Fore.RED + " (")
+    print("      (( || " + Style.RESET_ALL + "`--'  | _______ |  | " + Fore.RED + "    ))  (")
+    print("    (  )\|| (  )\ " + Style.RESET_ALL + "| - --- - | -| " + Fore.RED + "(  ( \  ))")
+    print("    (\/  || ))/ ( " + Style.RESET_ALL + "| -- - -- |  |" + Fore.RED + " )) )  \((")
+    print("     ( ()||((( ())" + Style.RESET_ALL + "|         |  |" + Fore.RED + "( (( () )\n")
+    print(Fore.RED +"⎯⎯∈" + Style.RESET_ALL + " @amartinsec / amartin@amartinsec.com " + Fore.RED + "⎯⎯∈")
+
+
+
+if __name__ == '__main__':
+     art()
+
+
